fa557a412dea8809450bc4b5bfd2742dd17cec92
1ri5 <iriscyst@gmail.com>
2022-09-07 11:18:15 +0200

    Access token + Refresh Token

--- server/router/authRouter.js
+++ server/router/authRouter.js
@@ -10,7 +10,6 @@ router.post('/login',authCtrl.login);
 
 router.post('/logout',authCtrl.logout);
 
-router.post('/refresh_token',authCtrl.generateAccesToken);
+router.post('/refresh_token',authCtrl.generateAccessToken);
 
 export default router;
-
