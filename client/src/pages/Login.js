5259ea76873f741d6b721e4eadcf003973715536
bitia <bitia_r_24@hotmail.com>
2022-09-08 14:04:51 +0200

    structure and css - Login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,35 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import '../styles/Login.css';
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <div>
-      <h3>login</h3>
-    </div>
-  )
-}
+    <div className="login">
+      <div className="login-container">
+      <h3 className="login-header">Social network</h3>
+      <h6 className="login-subheader">Login</h6>
+      
+        <form className="login-dataform" >
+          <input
+            className="data-formemail"
+            type="email"
+            value={email}
 
-export default Login
+            onChange={(e)=>setEmail(e.target.email)}
+            placeholder="type your email" />
+          <input
+            className="login-dataformpass"
+            type="password"
+            placeholder="type your password"
+           value={password}
+           onChange={(e)=>setPassword(e.target.password)}/>
+          <button
+          className="login-dataformbtn"
+            type="submit">Log In</button>
+        </form>
+        </div>
+    </div>
+  );
+};
+
+export default Login;
