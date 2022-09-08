cd817815648dcf4076eea47314b07fd91d8bcd6f
bitia <bitia_r_24@hotmail.com>
2022-09-08 18:06:16 +0200

    #1 Register structure and css

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
 import '../styles/Login.css';
+import { Link } from 'react-router-dom';
+
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+const [showpass, setShowpass] = useState(false)
+
   return (
     <div className="login">
       <div className="login-container">
@@ -14,18 +18,21 @@ const Login = () => {
             className="data-formemail"
             type="email"
             value={email}
-
-            onChange={(e)=>setEmail(e.target.email)}
+            onChange={(e)=>setEmail(e.target.value)}
             placeholder="type your email" />
           <input
             className="login-dataformpass"
-            type="password"
+            type={showpass ? "type" : "password"}
             placeholder="type your password"
            value={password}
-           onChange={(e)=>setPassword(e.target.password)}/>
+            onChange={(e) => setPassword(e.target.value)} />
+          
+          <small className="login-showpass" onClick={()=> setShowpass(!showpass) }>{showpass ? "Hide" : "show"} </small>
+
           <button
           className="login-dataformbtn"
             type="submit">Log In</button>
+          <p className="login-small">Do not have account <Link to="/register" >Create HERE</Link> </p>
         </form>
         </div>
     </div>
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,11 +1,89 @@
-import React from 'react'
+import React, { useState } from "react";
+import "../styles/Register.css";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  return (
-    <div>
-      <h2>Register</h2>
-    </div>
-  )
-}
+  const [username, setUsername] = useState("");
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [showcfpass, setShowcfpass] = useState(false);
+  const [showpass, setShowpass] = useState(false);
+  const[gender, setGender]= useState('male')
 
-export default Register
+  return (
+    <div className="register">
+      <div className="register-container">
+        <h3 className="register-header">Social network</h3>
+        <h6 className="register-subheader">REGISTER</h6>
+
+        <form className="register-dataform">
+          <input
+            className="data-formemail"
+            type="text"
+            value={fullname}
+            onChange={(e) => setFullname(e.target.value)}
+            placeholder="type your fullname"
+          />
+
+          <input
+            className="data-formemail"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="type your username"
+          />
+
+          <input
+            className="register-dataformpass"
+            type="email"
+            placeholder="type your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <input
+            className="register-dataformpass"
+            type={showcfpass ? "hide" : "password"}
+            placeholder="type your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <small
+            className="register-showcfpass"
+            onClick={() => setShowcfpass(!showcfpass)}
+          >
+            {showcfpass ? "Hide" : "show"}{" "}
+          </small>
+          <input
+            className="register-dataformpass"
+            type={showpass ? "type" : "password"}
+            placeholder="type your password again"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <select className="register-dataformselect" value={gender} onChange={(e) => setGender(e.target.value)}>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="other">Other</option>
+          </select>
+          <small
+            className="register-showpass"
+            onClick={() => setShowpass(!showpass)}
+          >
+            {showpass ? "Hide" : "show"}{" "}
+          </small>
+          <button className="register-dataformbtn" type="submit">
+            Log In
+          </button>
+          <p className="register-small">
+            Already have an account <Link to="/">LogIn HERE</Link>{" "}
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
