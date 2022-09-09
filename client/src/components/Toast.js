05885a520c4e4547e340240edf213a0047b74f85
bitia <bitia_r_24@hotmail.com>
2022-09-09 23:19:54 +0200

    change toast

--- client/src/components/Toast.js
+++ client/src/components/Toast.js
@@ -1,17 +1,49 @@
 import React from "react";
 
-const Toast = ({msg, handleShow, bgColor}) =>{
-    return(
-        <div className="toast" style={{position:"fixed", padding:"5px", borderRadius: "5px", backgroundColor: `${bgColor}`, color:"rgb(71, 60, 107)", top:"5px", right:"5px", zIndex:"50", maxWidth:"230px"}}>
-            <div style={{display: "flex", alignItems: "center", justifyContent: "space-between", borderBottom: "1px solid rgb(149, 149, 231)", padding: "0 rem .5rem"}} className="toast-header">
-                <h5 style={{fontWeight: "600"}}>{msg.title}</h5>
-                <p style={{fontSize: "1.5rem", alignSelf:"flex-start", cursor: "pointer"}}>&times;</p>
-            </div>
-            <div style={{padding: "0rem .5rem"}} className="toast-body">
-                <p style={{cursor: "pointer"}} onClick={handleShow}>{msg.body}</p>
-            </div>
-        </div>
-    )
-}
+const Toast = ({ msg, handleShow, bgColor }) => {
+  return (
+    <div
+      className="toast"
+      style={{
+        position: "fixed",
+        padding: "5px",
+        borderRadius: "5px",
+        backgroundColor: `${bgColor}`,
+        color: "rgb(71, 60, 107)",
+        top: "5px",
+        right: "5px",
+        zIndex: "50",
+        maxWidth: "230px",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          borderBottom: "1px solid rgb(149, 149, 231)",
+          padding: "0 rem .5rem",
+        }}
+        className="toast-header"
+      >
+        <h5 style={{ fontWeight: "600" }}>{msg.title}</h5>
+        <p
+          style={{
+            fontSize: "1.5rem",
+            alignSelf: "flex-start",
+            cursor: "pointer",
+          }}
+        >
+          &times;
+        </p>
+      </div>
+      <div style={{ padding: "0rem .5rem" }} className="toast-body">
+        <p style={{ cursor: "pointer" }} onClick={handleShow}>
+          {msg.body}
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default Toast;
