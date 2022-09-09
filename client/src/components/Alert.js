97c7074bf89d7ce123144da3371979b566bce62d
1ri5 <iriscyst@gmail.com>
2022-09-09 20:27:04 +0200

    #1 Alerts at login - Error/Success

--- client/src/components/Alert.js
+++ client/src/components/Alert.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux';
 import Loading from "./Loading";
+import Toast from './Toast';
 
 const Alert = () =>{
     const {auth, alert} = useSelector(state => state)
-    console.log({auth, alert})
+    const dispatch = useDispatch();
+
+    const close = () =>{
+        dispatch({
+            type:"ALERT",
+            payload:{}
+        })
+    }
+
     return(
         <div>
             {alert.loading && <Loading/>}
-            <h2>ALERT</h2>
+            {alert.error && <Toast msg={{title:"Error" ,  body:alert.error}} bgColor="rgb(222, 149, 162)"
+            handleShow={close} />}
+            {alert.success && <Toast msg={{title:"Success", body:alert.success}} bgColor="rgb(177, 222, 149)"
+            handleShow={close} />}
+            
         </div>
     )
 }
