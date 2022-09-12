ce462ff055873eff19a36d2d2f8ca8627d05ba23
bitia <bitia_r_24@hotmail.com>
2022-09-12 09:42:52 +0200

    #9 privateroute

--- client/src/utils/PrivateRouter.js
+++ client/src/utils/PrivateRouter.js
@@ -1,9 +1,9 @@
-import React from "react";
-import {Route, Navigate} from "react-router-dom"
+// import React from "react";
+// import {Route, Navigate} from "react-router-dom"
 
-const PrivateRouter = ({props, children}) =>{
-    const login = localStorage.getItem('login')
-    return login ? <Route {...props}>{children}</Route> : <Navigate to="/"></Navigate>
-}
+// const PrivateRouter = ({props, children}) =>{
+//     const login = localStorage.getItem('login')
+//     return login ? <Route {...props}>{children}</Route> : <Navigate to="/"></Navigate>
+// }
 
-export default PrivateRouter;
+// export default PrivateRouter;
