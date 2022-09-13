421ecf5a1a6e0cb2b368bde6b00fdefbf2e3dbfe
bysmartos <bysmartos@gmail.com>
2022-09-13 12:02:03 +0200

    #5 list of follwing and followers in profile

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -7,6 +7,15 @@ import {useSelector, useDispatch} from 'react-redux';
 import {useParams} from 'react-router-dom';
 import { getProfileUsers } from "../redux/actions/profileActions";
 import { useEffect, useState } from "react";
+import { IconButton } from "@mui/material";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import PeopleIcon from '@mui/icons-material/People';
+import BookmarksIcon from '@mui/icons-material/Bookmarks';
+import Friends from "../components/Friends";
+import Following from "../components/Following";
+// import SingleUserPosts from "../components/SingleUserPosts";
+// import SavedPost from "../components/SavedPost";
 
 
 
@@ -18,6 +27,36 @@ const Profile = () => {
   const { id } = useParams();
   const { auth, profile } = useSelector(state => state);
   const dispatch = useDispatch();
+  const [showaccount, setshowaccount] = useState(true);
+const [showfriends, setshowfriends] = useState(false);
+const [showfollowing, setshowfollowing] = useState(false);
+const [showsaved, setshowsaved] = useState(false);
+
+const handletoggle = (ht) =>{
+    
+    if(ht === 'showaccount'){
+        setshowsaved(false);
+        setshowfriends(false);
+        setshowfollowing(false);
+        setshowaccount(true)
+    }else if(ht === 'showfriends'){
+        setshowsaved(false);
+        setshowfriends(true);
+        setshowfollowing(false);
+        setshowaccount(false)
+    }else  if(ht === 'showfollowing'){
+        setshowsaved(false);
+        setshowfriends(false);
+        setshowfollowing(true);
+        setshowaccount(false)
+    }else  if(ht === 'showsaved'){
+        setshowsaved(true);
+        setshowfriends(false);
+        setshowfollowing(false);
+        setshowaccount(false)
+    }
+
+}
 
   useEffect(() => {
     if (auth && auth.user && id === auth.user._id){
@@ -32,6 +71,27 @@ const Profile = () => {
   return (
     <div className="profile">
       <Info userData={userData} profile={profile} auth={auth} id={ id} />
+      <div className ="profileheader">
+              <div className="profileheader-items">
+                  <IconButton className="profileheader-item" onClick={()=>handletoggle('showaccount')}>
+                        <AccountCircleIcon/>
+                  </IconButton>
+                  <hr/>
+                  <IconButton onClick={()=>handletoggle('showfriends')}>
+                        <PeopleIcon/>
+                  </IconButton>
+                  <hr/>
+                  <IconButton onClick={()=>handletoggle('showfollowing')}>
+                        <PersonAddIcon/>
+                  </IconButton>
+                  <hr/>
+                  <IconButton onClick={()=>handletoggle('showsaved')}>
+                        <BookmarksIcon/>
+                  </IconButton>
+                  
+              </div>
+        </div>  
+        { showaccount &&
             <div className="profilebody">
                 <div className='profilebody-left'>
                 <About userData={userData} profile={profile} auth={auth} id={ id}/>
@@ -44,6 +104,16 @@ const Profile = () => {
             </div>
          
               </div>
+}
+{
+    showfriends && <Friends userData={userData} dispatch={dispatch} profile={profile} auth={auth} id={id}/>
+}
+{
+    showfollowing && <Following  userData={userData} dispatch={dispatch} profile={profile} auth={auth} id={id}/>
+}
+{/* {
+    showsaved && <SavedPost auth={auth}  />
+} */}
         </div>  
   )
 }
