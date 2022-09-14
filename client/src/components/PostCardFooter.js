4059c781635a9322c41e4eff65cfb5b17de3dbb6
bysmartos <bysmartos@gmail.com>
2022-09-14 23:39:48 +0200

    #4 show user posts on profile

---
+++ client/src/components/PostCardFooter.js
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from 'react';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+
+import CommentIcon from '@mui/icons-material/Comment';
+import SaveAltIcon from '@mui/icons-material/SaveAlt';
+import {Link} from "react-router-dom"
+import "../styles/PostCard.css"
+
+import {useSelector, useDispatch} from "react-redux"
+import {unlikepost, likepost,savedPost, unsavedPost} from "../redux/actions/postActions"
+
+const PostCardFooter = ({pos}) =>{
+    const [isLike, setIsLike] = useState(false)
+    const [load, setLoad] = useState(false)
+    const [saved, setSaved]= useState(false)
+    const {auth, socket} = useSelector(state => state);
+    const dispatch = useDispatch();
+
+    // useEffect(() => {
+    //     if(pos.likes.find(like => like._id === auth.user._id)){
+    //         setIsLike(true)
+    //     }else{
+    //         setIsLike(false)
+    //     }
+    // },[pos.likes, auth.user._id])
+    // useEffect(() => {
+    //     if(auth.user.saved.find(id => id === pos._id)){
+    //         setSaved(true)
+    //     }else{
+    //         setSaved(false)
+    //     }
+    // },[pos._id, auth.user.saved])
+    // const handleLike = async () =>{
+    //     if(load) return;
+    //     setIsLike(true)
+    //     setLoad(true)
+    //     dispatch(likepost({pos, auth, socket}))
+    //     setLoad(false)
+    // }
+    // const handleUnLike = async () =>{
+    //     if(load) return;
+    //     setIsLike(false)
+    //     setLoad(true)
+    //     dispatch(unlikepost({pos, auth, socket}))
+    //     setLoad(false)
+        
+    // }
+
+
+    return (
+        <div className="postcardfooter">
+            <div className="postcardfootertop">
+                <div className="postcardfootertopitems">
+                   <span> {pos.likes?.length} </span> 
+                   <FavoriteBorderIcon style={{color:'red'}}/>
+                </div>
+                <div className="postcardfootertopitems">
+                <span> {pos.commentss?.length} </span> 
+                <CommentIcon/>
+                </div>
+            </div>
+            <div className="postcardfooterbottom">
+                <div className="postcardfooterbottomitems">
+                <p> Favorite </p>
+                </div>
+                <Link to={`/post/${pos._id}`}>
+                <div className="postcardfooterbottomitems">
+                <CommentIcon/>
+                <p> Opinion </p>
+                </div>
+                </Link>
+                <div className="postcardfooterbottomitems">
+                { saved 
+                    ?<SaveAltIcon style={{color:'rebeccapurple'}} onClick={()=>dispatch(unsavedPost({pos, auth}))}/>
+                    :<SaveAltIcon onClick={()=> dispatch(savedPost({pos, auth}))}/>
+                    }
+                <p> Save </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PostCardFooter;
--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React from 'react';
+import {useSelector} from "react-redux"
 
-const Posts = () => {
-  return (
-    <div className="profileposts">
-      <h2>Posts</h2>
-    </div>
-  );
-};
+import PostCardBody from './PostCardBody';
+import PostCardFooter from './PostCardFooter';
+import PostCardHeader from './PostCardHeader';
+
+
+const Posts = () =>{
+    const {homePost} = useSelector(state=>state)
+
+    return(
+        <div className="posts">
+            {
+                homePost && homePost.post.length > 0 && homePost.post.map((pos)=>(
+                    <div className="postCards" key={pos._id}style={{backgroundColor:'white',padding:'1rem', marginTop:'1rem', borderRadius:'10px', boxShadow:'3px 3px 5px gray',width:'500px'}} >
+                        <PostCardHeader pos={pos}/>
+                        <PostCardBody pos={pos}/>
+                        <PostCardFooter pos={pos}/>
+                   
+                    
+                    </div>
+                ))
+            }
+            
+        </div>
+    )
+}
 
 export default Posts;
