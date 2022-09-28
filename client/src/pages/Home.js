7fb3c861a3406d266275c51df684fa8540f38336
1ri5 <iriscyst@gmail.com>
2022-09-28 12:57:59 +0200

    #5 #6 Responsive fix

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,6 +6,7 @@ import { Grid } from "@mui/material";
 import Status from "../components/Status";
 import PostAll from "../components/PostAll";
 import PostMid from "../components/PostMid";
+import "../styles/Home.css"
 
 const Home = () => {
   return (
@@ -18,20 +19,24 @@ const Home = () => {
             justifyContent:"space-around",
             alignItems:"flex-start",
             className:"profilebody"}}>
-        <Grid item sx={12} md={3} className="homemain-contentleft" style={{paddingLeft: "5%"}}>
+        {/* <Grid item sx={12} md={3} className="homemain-contentleft" style={{paddingLeft: "5%"}}> */}
+        <div className="homemain-contentleft">
           <HomeLeft />
-        </Grid>
-        <Grid item sx={12} md={6} 
+          </div>
+        {/* </Grid> */}
+
+        {/* <Grid item sx={12} md={6} 
           className="homemain-contentmid"
           style={{width: "100%", maxWidth: "32.5rem"}}
-        >
+        > */}
+        <div className="homemain-contentmid">
           <PostMid/>
-          {/* <Status/> */}
-          {/* <PostAll /> */}
-        </Grid>
-        <Grid item md={3}  className="homemain-contentright">
-              
-                </Grid>
+</div>
+        {/* </Grid> */}
+        {/* <Grid item md={3}  className="homemain-contentright">
+                </Grid> */}
+                <div className="homemain-contentright">
+                </div>
       </Grid>
     </div>
   );
