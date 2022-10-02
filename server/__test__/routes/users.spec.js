cd0a650ca6d115e037efd104d64f2f4ec1c36683
bysmartos <bysmartos@gmail.com>
2022-10-02 20:01:11 +0200

    #3 button paneladmin only show on your admin profile, not on all admin profiles

--- server/__test__/routes/users.spec.js
+++ server/__test__/routes/users.spec.js
@@ -1,21 +1,41 @@
 import express from 'express';
+import mongoose from "mongoose";
 import request from 'supertest';
-import  userRoute  from '../../router/userRouter';
+// import app from '../../server.js';
+// import jest from 'jest';
+// import  userRoute  from '../../router/userRouter.js';
+import  authRoute  from '../../router/authRouter.js';
 
 
-
+// const api=supertest(app)
 const app = express();
 
 app.use(express.json())
-app.use(cors());
-app.use(cookieparser());
-app.use(userRoute);
+// app.use(userRoute);
+app.use(authRoute);
 
-describe('/users', ()=>
+
+
+describe('/login', ()=>
 test('should return status 200 & content-type "application/json"',async ()=>{
-    const response = await request(app)
-    .get('/users')
-    .set({ Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMzg4ODM4ZDY2ODBkMzJhZTdhNTQ5NyIsImlhdCI6MTY2NDY2NjI5MiwiZXhwIjoxNjY0NzUyNjkyfQ.CcodKc77V4hD46RtXjLOZmOwd4B-Q7BPnwfS1sHGekw' })
+//  await api
+const response = await request(app)
+    .post('/login')
+    .send({ 
+        email: "developer2@mail.com",
+        password: "123456"  })
     expect(response.status).toBe(200);
     expect(response.headers['content-type']).toContain('application/json')
 }))
+// describe('/users', ()=>
+// test('should return status 200 & content-type "application/json"',async ()=>{
+//     const response = await request(app)
+//     .get('/users')
+//     .set({ Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMzg4ODM4ZDY2ODBkMzJhZTdhNTQ5NyIsImlhdCI6MTY2NDY2NjI5MiwiZXhwIjoxNjY0NzUyNjkyfQ.CcodKc77V4hD46RtXjLOZmOwd4B-Q7BPnwfS1sHGekw' })
+//     expect(response.status).toBe(200);
+//     expect(response.headers['content-type']).toContain('application/json')
+// }))
+
+afterAll(()=>{
+    mongoose.connection.close();
+})
