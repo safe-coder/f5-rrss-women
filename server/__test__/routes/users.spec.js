3c2db02bfdafa159ab7f834e79c3530f9d8b3166
bysmartos <bysmartos@gmail.com>
2022-10-03 12:28:46 +0200

    testing server working done login and users test

--- server/__test__/routes/users.spec.js
+++ server/__test__/routes/users.spec.js
@@ -1,41 +1,26 @@
 import express from 'express';
-import mongoose from "mongoose";
 import request from 'supertest';
-// import app from '../../server.js';
-// import jest from 'jest';
-// import  userRoute  from '../../router/userRouter.js';
-import  authRoute  from '../../router/authRouter.js';
-
-
-// const api=supertest(app)
-const app = express();
-
-app.use(express.json())
-// app.use(userRoute);
-app.use(authRoute);
-
+import app from '../../server.js';
 
 
 describe('/login', ()=>
 test('should return status 200 & content-type "application/json"',async ()=>{
-//  await api
+
 const response = await request(app)
-    .post('/login')
+    .post('/api/login')
     .send({ 
         email: "developer2@mail.com",
-        password: "123456"  })
+        password: "444444"  })
     expect(response.status).toBe(200);
     expect(response.headers['content-type']).toContain('application/json')
 }))
-// describe('/users', ()=>
-// test('should return status 200 & content-type "application/json"',async ()=>{
-//     const response = await request(app)
-//     .get('/users')
-//     .set({ Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMzg4ODM4ZDY2ODBkMzJhZTdhNTQ5NyIsImlhdCI6MTY2NDY2NjI5MiwiZXhwIjoxNjY0NzUyNjkyfQ.CcodKc77V4hD46RtXjLOZmOwd4B-Q7BPnwfS1sHGekw' })
-//     expect(response.status).toBe(200);
-//     expect(response.headers['content-type']).toContain('application/json')
-// }))
 
-afterAll(()=>{
-    mongoose.connection.close();
-})
+describe('/api/users', ()=>
+test('should return status 200 & content-type "application/json"',async ()=>{
+    const response = await request(app)
+    .get('/api/users')
+    .set({ Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMzk0ZDhjNzU3ZTIxOTllYTgyZTk1YSIsImlhdCI6MTY2NDc4OTU1NCwiZXhwIjoxNjY0ODc1OTU0fQ.c8k5KcPaMbxC0sQfQ3O691INHP7mWVpe1ErHzmN0kWo' })
+    expect(response.status).toBe(200);
+    expect(response.headers['content-type']).toContain('application/json')
+}))
+
