2a91df0f610ba23c14e0507fcfeb3c986b362d48
bysmartos <bysmartos@gmail.com>
2022-10-03 17:33:12 +0200

    start with auth testing on server, not totally done, only login route

--- server/__test__/routes/users.spec.js
+++ server/__test__/routes/users.spec.js
@@ -21,7 +21,7 @@ describe(" GET /api/search", () => {
 });
 
 describe(" GET /api/users", () => {
-    describe("/api/users", () =>
+    describe("when passed all info", () =>
     test('should return status 200 & content-type "application/json"', async () => {
       const response = await request(app)
         .get("/api/users")
@@ -38,11 +38,11 @@ describe(" GET /api/users", () => {
       }));
   });
 
-  describe(" GET /api/users", () => {
-    describe("/api/users", () =>
+  describe(" GET /api/user/:id", () => {
+    describe("when passed all info", () =>
     test('should return status 200 & content-type "application/json"', async () => {
       const response = await request(app)
-        .get("/api/users")
+        .get(`/api/user/${process.env.USER_ID}`)
         .set({ Authorization: process.env.BEARER });
       expect(response.status).toBe(200);
       expect(response.headers["content-type"]).toContain("application/json");
@@ -50,12 +50,69 @@ describe(" GET /api/users", () => {
     describe("when some info is missing", () =>
       test('should return a 500 status code to show there was a user error', async () => {
         const response = await request(app)
-        .get("/api/users")
-        .set({ Authorization: " " });
+        .get(`/api/user/test`)
+        .set({ Authorization: process.env.BEARER });
         expect(response.status).toBe(500);
       }));
   });
 
+  //AUTH_ROUTES_TEST
+
+  
 
 
+// router.post('/logout',authCtrl.logout);
+
+// router.post('/refresh_token',authCtrl.generateAccessToken);
+
+
+describe(" POST /api/login", () => {
+    describe('when passed all info', ()=>
+    test('should return status 200 & content-type "application/json"',async ()=>{
+    
+    const response = await request(app)
+        .post('/api/login')
+        .send({ 
+            email: "developer@mail.com",
+            password: "123456"  })
+        expect(response.status).toBe(200);
+        expect(response.headers['content-type']).toContain('application/json')
+    }))
+    
+    
+    describe("when some info is missing", () =>
+      test('should return a 500 status code to show there was a user error', async () => {
+        const response = await request(app)
+        .post('/api/login')
+        .send({ 
+            email: "developer@mail.com",
+            password: ""  })
+        expect(response.status).toBe(400);
+      }));
+  });
+
+  describe(" POST /api/login", () => {
+    describe('when passed all info', ()=>
+    test('should return status 200 & content-type "application/json"',async ()=>{
+    
+    const response = await request(app)
+        .post('/api/login')
+        .send({ 
+            email: "developer@mail.com",
+            password: "123456"  })
+        expect(response.status).toBe(200);
+        expect(response.headers['content-type']).toContain('application/json')
+    }))
+    
+    
+    describe("when some info is missing", () =>
+      test('should return a 500 status code to show there was a user error', async () => {
+        const response = await request(app)
+        .post('/api/login')
+        .send({ 
+            email: "developer@mail.com",
+            password: ""  })
+        expect(response.status).toBe(400);
+      }));
+  });
 
